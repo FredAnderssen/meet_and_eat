06bc7d93108681a79c67d90897a770d6e75aa6ed
FredLundberg <fredric.idk@outlook.com>
2019-02-05 16:47:30 +0100

    addded DB yml

--- web_application/src/app.js
+++ web_application/src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function(request, response) {
-    response.send("Hello,Lundberg Andersson")
+    response.send("Hello, Andersson")
 })
 
 app.listen(8080, function() {
