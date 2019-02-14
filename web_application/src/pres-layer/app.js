e3a83580ecf67dd98457e352c1b22b26e5409c79
Joakim <brjo1621@student.ju.se>
2019-02-14 16:39:17 +0100

    Successfully restarted project

--- web_application/src/pres-layer/app.js
+++ web_application/src/pres-layer/app.js
@@ -3,8 +3,6 @@ const expressHandlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const path = require('path')
 
-const dummyData = require('../data-access-layer/dummyData')
-
 const app = express()
 
 app.set('views', path.join(__dirname, 'views'))
@@ -20,11 +18,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static('public_html'))
 
 app.get('/', function(request, response) {
-  const model = {
-    restaurants: dummyData.restaurants
-  }
-
-    response.render('index.hbs', model)
+  response.render('index.hbs')
 })
 
 app.listen(8080, function() {
