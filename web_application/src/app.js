372d2c8aa5a4d3dace3e93ad5b3586eaffc13525
Joakim <brjo1621@student.ju.se>
2019-02-14 15:51:50 +0100

    Cleaned up code, fresh start

--- web_application/src/app.js
+++ web_application/src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function(request, response) {
-    response.send("Hello, Andersson")
+    response.send("Hello, updated name")
 })
 
 app.listen(8080, function() {
