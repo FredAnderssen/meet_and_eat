fc364666ef98b3d23ea860ce9eae5948580f271e
FredLundberg <fredric.idk@outlook.com>
2019-03-08 14:53:55 +0100

    added rest api CRUDs and token and restructured the layers, validation of tokens is needed to do

--- web_application/src/pres-layer/main.js
+++ web_application/src/pres-layer/main.js
@@ -1,27 +1,46 @@
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+var mysql = require('mysql');
+
+const mainApp = express()
 
 const awilix = require('awilix')
-const accountRouterFun = require('./routers/account-router.js')
-const container = awilix.createContainer()
-container.register('accountRouter', awilix.asFunction(accountRouterFun))
-const accountRouter = container.resolve('accountRouter')
-
-//listen app here
-//calla denna fil nånstans
-
-const awilix = require('awilix')
-
-// Import the ones we want to use (real or mockup), real in this case.
+const accountRouter = require('./routers/account-router')
 const accountRepository = require('../data-layer/repositories/account-repository')
 const accountManager = require('../bus-layer/managers/account-manager')
-const accountRouter = require('./routers/account-router')
-//.. Fler dependencies
+const accountValidator = require('../bus-layer/managers/account-validator')
+const crypt = require('../bus-layer/utilities/crypt')
 
+const cardsRouter = require('./routers/cards-router')
+const cardsRepository = require('../data-layer/repositories/cards-repository')
+const cardsManager = require('../bus-layer/managers/cards-manager')
 
-// Create a container and add the dependencies we want to use.
+var apiApp = require('../api-pres-layer/api'); //TODO DI=?
+var app = require('./app')
+
 const container = awilix.createContainer()
 
+container.register('accountRouter', awilix.asFunction(accountRouter))
 container.register("accountRepository", awilix.asFunction(accountRepository))
 container.register("accountManager", awilix.asFunction(accountManager))
-container.register("accountRouter", awilix.asFunction(accountRouter))
-// Retrieve the router, which resolves all other dependencies.
-const theAccountRouter = container.resolve("accountRouter")
+container.register("accountValidator", awilix.asFunction(accountValidator))
+
+container.register("crypt", awilix.asFunction(crypt))
+
+container.register('cardsRouter', awilix.asFunction(cardsRouter))
+container.register('cardsRepository', awilix.asFunction(cardsRepository))
+container.register('cardsManager', awilix.asFunction(cardsManager))
+
+container.register('apiApp', awilix.asFunction(apiApp)) //TODO DI?
+container.register('app', awilix.asFunction(app))
+
+const theApiApp = container.resolve('apiApp')
+const theApp = container.resolve('app')
+
+mainApp.use('/api', theApiApp)
+mainApp.use('/', theApp)
+
+mainApp.listen(8080, () => console.log('Good to go on 3000!'))
