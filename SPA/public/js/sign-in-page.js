cd324047a2ff1dfd9dff7630172784f65a043442
FredLundberg <fredric.idk@outlook.com>
2019-03-18 16:23:25 +0100

    added login to SPA

---
+++ SPA/public/js/sign-in-page.js
@@ -0,0 +1,40 @@
+var userInfo
+var accessToken
+
+document.addEventListener("DOMContentLoaded", function(){
+
+	const signInForm = document.querySelector("#sign-in-page form")
+
+	signInForm.addEventListener("submit", function(event){
+		event.preventDefault();
+
+		const username = document.getElementById("sign-in-username").value
+		const password = document.getElementById("sign-in-password").value
+
+		fetch("http://localhost:3000/api/tokens", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded"
+			},
+			body: "grant_type=password&username="+encodeURI(username)+"&password="+encodeURI(password)
+		}).then(function(response){
+			return response.json()
+		}).then(function(body){
+
+			accessToken = body.access_token
+			const idToken = body.id_token
+
+			userInfo = jwt_decode(idToken)
+
+			document.querySelector("nav").classList.add("user-is-logged-in")
+			document.getElementById("username").innerText = userInfo.preferred_username
+
+			changePage("/show-cards-page")
+
+		}).catch(function(error){
+			console.log(error)
+		})
+
+	})
+
+})
